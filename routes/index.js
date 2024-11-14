91d40c84954e745e43ed32b156fcf2ea5c5db39a
Thomas INIGUEZ-VISIOLI <tiniguezvi@290070u.local>
2024-11-14 16:39:04 +0100

    source

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,13 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express', users:[
+    {
+    link:"/",name:"bulbe"
+  },{
+    link:"/",name:"bulbe2"
+  }
+] });
 });
 
 module.exports = router;
